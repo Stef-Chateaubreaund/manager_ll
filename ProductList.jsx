c3839558ccf3c2ce0daa147b859fef34e909badf
Stef Chateaubreaund <103604314+Stef-Chateaubreaund@users.noreply.github.com>
2022-07-19 10:31:13 -0400

    Add files via upload

---
+++ ProductList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+    
+const ProductList = (props) => {
+    const { removeFromDom } = props;
+    
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(response => {
+                removeFromDom(productId)
+            })
+            .catch(err => console.error(err));
+    }
+    
+    return (
+        <div>
+            {props.product.map((product, idx) => {
+                return <p key={idx}>
+                    <Link to={"/" + product._id}>
+                        {product.title}
+                    </Link>
+                    |
+                    <button onClick={(e)=>{deleteProduct(product._id)}}>
+                        Delete
+                    </button>
+                </p>
+            })}
+        </div>
+    )
+}
+    
+export default ProductList;
+
